2ce21ad35a7b7b76066c0be3ca5d7242564ed63e
tpl-86 <tron-050686@mail.ru>
2025-05-22 20:31:55 +0300

    Create class Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title
