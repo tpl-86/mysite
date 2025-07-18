099755119844f8feed6659fce3962692afbc1e3f
tpl-86 <tron-050686@mail.ru>
2025-07-18 08:31:37 +0300

    Added static.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
+        
 
 class Post(models.Model):
 
@@ -8,7 +17,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='blog_posts', null=True, blank=True)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -16,12 +28,23 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta():
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
         ]
+    
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
